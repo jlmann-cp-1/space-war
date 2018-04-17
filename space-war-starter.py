8be12c417a5be2c0fb74d50099a4897a75a345d2
Jon Cooper <joncoop@users.noreply.github.com>
2018-04-17 14:28:40 -0400

    Add files via upload

---
+++ space-war-starter.py
@@ -0,0 +1,68 @@
+# Imports
+import pygame
+
+# Initialize game engine
+pygame.init()
+
+
+# Window
+WIDTH = 800
+HEIGHT = 600
+SIZE = (WIDTH, HEIGHT)
+TITLE = "Space War"
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption(TITLE)
+
+
+# Timer
+clock = pygame.time.Clock()
+refresh_rate = 60
+
+# Colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+GREEN = (100, 255, 100)
+
+# Images
+
+
+
+# Game classes
+
+
+    
+# Make game objects
+
+
+
+# Game loop
+done = False
+
+while not done:
+    # Event processing (React to key presses, mouse clicks, etc.)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        
+    
+    # Game logic (Check for collisions, update points, etc.)
+   
+
+        
+    # Drawing code (Describe the picture. It isn't actually drawn yet.)
+    screen.fill(BLACK)
+
+    
+    # Update screen (Actually draw the picture in the window.)
+    pygame.display.flip()
+
+
+    # Limit refresh rate of game loop 
+    clock.tick(refresh_rate)
+
+
+# Close window and quit
+pygame.quit()
